536110ca04678c310407087c1ce999ac2f11d8ff
yongcai <yongcai@tencent.com>
2020-09-23 09:46:12 +0800

    update(raft): add file info to log

--- 6.824/raft/util.go
+++ 6.824/raft/util.go
@@ -2,11 +2,13 @@ package raft
 
 import (
 	"flag"
+	"fmt"
 	"log"
 )
 
 func init() {
 	flag.IntVar(&Debug, "debug", 0, "debug log")
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 }
 
 // Debugging
@@ -14,7 +16,8 @@ var Debug = 0
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
-		log.Printf(format, a...)
+		log.Output(2, fmt.Sprintf(format, a...))
+		//log.Printf(format, a...)
 	}
 	return
 }
